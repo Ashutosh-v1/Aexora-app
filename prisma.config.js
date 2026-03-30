b76bd35d22d07fe9283800b7737914b7ccb1a9ed
Ashutosh Verma <145222383+Ashutosh-v1@users.noreply.github.com>
2026-03-30 23:57:10 +0530

    resolved error

--- prisma.config.js
+++ prisma.config.js
@@ -3,9 +3,4 @@ require("dotenv/config");
 
 module.exports = defineConfig({
   schema: "prisma/schema.prisma",
-
-  datasource: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
-  },
 });
