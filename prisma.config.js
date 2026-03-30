ff5974290a5c6cca1776d799438568b52f100780
Ashutosh Verma <145222383+Ashutosh-v1@users.noreply.github.com>
2026-03-31 00:04:06 +0530

    due to deployment issue|| removed adapter || but now again reinstalled it for local working

--- prisma.config.js
+++ prisma.config.js
@@ -3,4 +3,9 @@ require("dotenv/config");
 
 module.exports = defineConfig({
   schema: "prisma/schema.prisma",
+
+  datasource: {
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
+  },
 });
